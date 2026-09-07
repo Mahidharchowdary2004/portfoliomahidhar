9f530a74c3ea74c3ff58553b02143c7fb93ee236
Mahidharchowdary2004 <mahidharchowdary2004@users.noreply.github.com>
2026-09-07 21:07:55 +0530

    DB

--- portfolio-backend/src/server.ts
+++ portfolio-backend/src/server.ts
@@ -1,4 +1,6 @@
 import 'dotenv/config';
+import dns from 'dns';
+dns.setServers(['8.8.8.8', '8.8.4.4']);
 import express, { Request, Response, NextFunction } from 'express';
 import cors from 'cors';
 import path from 'path';
